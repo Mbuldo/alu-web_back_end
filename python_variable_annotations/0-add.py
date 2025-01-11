be56b67f56e8d6fe287e42abfc157d11a54e554c
Mbuldo <m.buldo@alustudent.com>
2025-01-11 07:48:33 +0000

    Commit

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
+"""Module for adding float numbers with type annotations."""
+
+
 def add(a: float, b: float) -> float:
+    """Return the sum of two float numbers.
+
+    Args:
+        a (float): The first float number
+        b (float): The second float number
+
+    Returns:
+        float: The sum of a and b
+    """
     return a + b
 

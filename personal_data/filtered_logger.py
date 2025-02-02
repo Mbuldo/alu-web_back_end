65928299759bf36f92baaf5850f852d8f4f4494f
Mbuldo <m.buldo@alustudent.com>
2025-02-02 12:19:43 +0000

    Done

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 Module for filtering sensitive information from log messages
 """
 import re
+import logging
 from typing import List
 
 
@@ -22,3 +23,30 @@ def filter_datum(fields: List[str], redaction: str,
     """
     pattern = f'({"|".join(fields)})=[^{separator}]*'
     return re.sub(pattern, f'\\1={redaction}', message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Format log records while redacting sensitive fields.
+
+        Args:
+            record: LogRecord instance containing log information
+
+        Returns:
+            Formatted log string with sensitive information redacted
+        """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
